6d947ca94d165647f0b81390b02262d858940757
Afonso Pereira <afonso.pereira@adene.pt>
2022-11-16 18:11:24 +0000

    Added: README.md Fixed: diretivas @click passaram a x-on:click para funcionar com o Alpine

--- resources/js/components/companies/CompaniesEdit.vue
+++ resources/js/components/companies/CompaniesEdit.vue
@@ -2,7 +2,7 @@
     <div v-if="errors">
       <div v-for="(v, k) in errors" :key="k" class="bg-red-500 text-white rounded font-bold mb-4 shadow-lg py-2 px-4 pr-0">
         <p v-for="error in v" :key="error" class="text-sm">
-          {{ error }}
+            {{ error }}
         </p>
       </div>
     </div>
